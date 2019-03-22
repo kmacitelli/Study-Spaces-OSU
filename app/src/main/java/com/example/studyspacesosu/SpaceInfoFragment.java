3f1780a15d902c65bf68709c7fa5e3c8806da11b
Kiera Acitelli <kacitelli@gmail.com>
2019-03-22 19:21:10 -0400

    delete unused imports

--- app/src/main/java/com/example/studyspacesosu/SpaceInfoFragment.java
+++ app/src/main/java/com/example/studyspacesosu/SpaceInfoFragment.java
@@ -5,23 +5,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 
 import java.util.HashMap;
@@ -30,11 +22,8 @@ import java.util.Map;
 
 public class SpaceInfoFragment extends Fragment {
 
-    private TextView mCoordinateView;
     private TextView mAreaName;
     private TextView mAreaDescription;
-    private Button mSubmitButton;
-    private Button mDeleteButton;
     private FirebaseFirestore mDatabase;
     static private Button editButton;
     private Button xButton;
